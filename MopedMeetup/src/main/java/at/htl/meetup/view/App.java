51606e8b589a2f9867edad3270607d98f0257997
SaidNurceski <saidnurceski@gmail.com>
2023-05-26 23:55:02 +0200

    created and updated: logged-in view, styling.css finished

--- MopedMeetup/src/main/java/at/htl/meetup/view/App.java
+++ MopedMeetup/src/main/java/at/htl/meetup/view/App.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/login-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/logged-in-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 700, 550);
         stage.setTitle("Hello!");
         stage.setScene(scene);
