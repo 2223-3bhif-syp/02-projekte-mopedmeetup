d866d935712628d16f8df0605ac962af05de34ed
SaidNurceski <saidnurceski@gmail.com>
2023-05-28 19:26:45 +0200

    added changeScene to DButils

--- MopedMeetup/src/main/java/at/htl/meetup/view/DBUtils.java
+++ MopedMeetup/src/main/java/at/htl/meetup/view/DBUtils.java
@@ -3,11 +3,37 @@ package at.htl.meetup.view;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.awt.event.ActionEvent;
 import java.io.IOException;
 
 public class DBUtils {
+    public static void changeScene(ActionEvent event, String fxmlFile, String title, String firstName){
+        Parent root = null;
 
+        if(firstName != null){
+            try{
+                FXMLLoader loader = new FXMLLoader(DBUtils.class.getResource(fxmlFile));
+                root = loader.load();
+                HomeController homeController = loader.getController();
+                homeController.setUserInformation(firstName);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        else{
+            try{
+                root = FXMLLoader.load(DBUtils.class.getResource(fxmlFile));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setTitle(title);
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
 }
--- MopedMeetup/src/main/java/at/htl/meetup/view/RegistrationController.java
+++ MopedMeetup/src/main/java/at/htl/meetup/view/RegistrationController.java
@@ -1,6 +1,9 @@
 package at.htl.meetup.view;
 
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
 import java.net.URL;
@@ -10,6 +13,12 @@ import  javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 
 public class RegistrationController {
+    public PasswordField tf_password;
+    public PasswordField tf_conf_password;
+    public TextField tf_firstname;
+    public TextField tf_email;
+    public Button button_login;
+
     public void close_app(MouseEvent event) {
         System.exit(0);
     }
