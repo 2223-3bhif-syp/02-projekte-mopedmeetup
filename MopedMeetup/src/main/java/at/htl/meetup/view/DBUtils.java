e21dd49da3292798f43e58395dcedc00f53f8abc
SaidNurceski <saidnurceski@gmail.com>
2023-05-28 18:44:46 +0200

    fixed: inserts and created: DBUtils for login with database connection

---
+++ MopedMeetup/src/main/java/at/htl/meetup/view/DBUtils.java
@@ -0,0 +1,13 @@
+package at.htl.meetup.view;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+
+public class DBUtils {
+
+}
--- MopedMeetup/src/main/java/at/htl/meetup/view/LoginController.java
+++ MopedMeetup/src/main/java/at/htl/meetup/view/LoginController.java
@@ -1,29 +1,13 @@
 package at.htl.meetup.view;
 
-import javafx.beans.binding.StringBinding;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.collections.ListChangeListener;
-import javafx.collections.transformation.FilteredList;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.Parent;
-import java.net.URL;
-import javafx.scene.control.*;
+
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
-import javafx.stage.WindowEvent;
-
 import java.io.IOException;
-import java.sql.SQLException;
-import java.util.EventListener;
-import java.util.Objects;
-import java.util.ResourceBundle;
 
 public class LoginController {
     @FXML
